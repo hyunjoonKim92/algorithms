f6874dc3ed1882d2d4c876e71832216f3946b0f0
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-07-12 20:31:59 +0900

    -- 220712 커밋 --- 핸드폰 번호 가리기 --- 하샤드 수

---
+++ solution/harshadNumber.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// 하샤드 수 (프로그래머스)
+function solution(x) {
+
+    let answer = true;
+    let sum = 0;
+    let arr = String(x).split("");
+    
+    arr.forEach(num => {
+        sum += Number(num); 
+    });
+
+    answer = x % sum === 0 ? true : false;
+
+    return answer;
+}
