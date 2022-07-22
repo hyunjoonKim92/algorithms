29772153a09402547a8c325dc5b4c22393bbb09a
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-07-22 19:27:17 +0900

    -- 220722 커밋 --- 약수의 합

---
+++ solution/sumFactor.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// 약수의 합 (프로그래머스)
+function solution(n) {
+
+    let answer = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            answer += i;
+        }
+    }
+
+    return answer;
+}
