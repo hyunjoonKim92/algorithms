dc8f08359ea7b231858f727c391b578940dddc29
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-07-25 18:44:32 +0900

    -- 220725 커밋 --- 디자인 패턴 ---- 싱글톤 패턴 ---- 팩토리 패턴

---
+++ designPattern/factory.js
@@ -0,0 +1,38 @@
+"use strict"
+
+// 팩토리 패턴
+class Car {
+    constructor(info) {
+        this.name = info.name;
+        this.price = info.price;
+    }
+
+    getInfo() {
+        return this.name + "의 가격은 " + this.price + " 입니다.";
+    }
+
+    static factory(name) {
+        if (name === "Avante") {
+            return new Avante();
+        } else if (name === "Sonata") {
+            return new Sonata();
+        }
+    }
+}
+
+class Sonata extends Car {
+    constructor() {
+        super({name: "쏘나타", price: "2,386 ~ 3,367만원"});
+    }
+}
+
+class Avante extends Car {
+    constructor() {
+        super({name: "아반떼", price: "1,570 ~ 2,453만원"});
+    }
+}
+
+const avante = Car.factory("Avante");
+const sonata = Car.factory("Sonata");
+
+console.log(avante, sonata);
---
+++ designPattern/singleTone.js
@@ -0,0 +1,37 @@
+"use strict"
+
+// 싱글톤 패턴
+const singleTone = (function() {
+
+    let instance;
+
+    function init() {
+        function privateMethod() {
+            console.log('I am private');
+        }
+
+        let privateVariable = 'I am also private';
+
+        return {
+            publicMethod: function() {
+                console.log('public Mentod');
+            },
+            publicProperty: 'public Property',
+        };
+    }
+
+    return {
+        getInstance: function() {
+            if (!instance) {
+                instance = init();
+            }
+
+            return instance;
+        }
+    }
+})();
+
+const first = singleTone.getInstance();
+const second = singleTone.getInstance();
+
+console.log(first === second);
