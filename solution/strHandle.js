2970ad71e0201452ccccbd4cfff97cf26aee5c0f
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-02 18:19:17 +0900

    -- 220802 커밋 --- 문자열 다루기 기본

---
+++ solution/strHandle.js
@@ -0,0 +1,21 @@
+"use strict"
+
+// 문자열 다루기 기본 (프로그래머스)
+function solution(s) {
+
+    let answer = true;
+
+    if (s.length === 4 || s.length === 6) {
+        s.toString().split("").forEach(item => {
+            let ascii = item.charCodeAt();
+
+            if (ascii < 48 || ascii > 57) {
+                answer = false;
+            }
+        });   
+    } else {
+        answer = false;
+    }
+
+    return answer;
+}
