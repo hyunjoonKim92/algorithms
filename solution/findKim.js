b66117b8fe98671414e45b4d734ce3ed95a93a33
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-08 17:34:11 +0900

    -- 220808 커밋 --- 서울에서 김서방 찾기 --- 문자열 내림차순으로 배치하기 --- 문자열 내 p와 y의 개수 --- 문자열 내 마음대로 정렬하기 --- 두 정수 사이의 합

---
+++ solution/findKim.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// 서울에서 김서방 찾기 (프로그래머스)
+function solution(seoul) {
+
+    let answer = '김서방은 ';
+    let idx;
+
+    seoul.forEach((item, index) => {
+         if (item === "Kim") {
+             idx = index;
+         }
+    });
+
+    return answer + idx.toString() +  '에 있다';
+}
+
+// 개선된 함수
+function improvedSolution(seoul) {
+
+    let idx = seoul.indexOf("Kim");
+
+    return "김서방은 " + idx.toString() + "에 있다"; 
+}
