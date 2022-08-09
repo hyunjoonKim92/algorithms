94b558d81a984ec15604fcc8ccb8c0ff36243091
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-09 17:33:09 +0900

    -- 220809 커밋 --- 나누어 떨어지는 숫자 배열

---
+++ solution/divisibleNumArr.js
@@ -0,0 +1,32 @@
+"use strict"
+
+// 나누어 떨어지는 숫자 배열 (프로그래머스)
+function solution(arr, divisor) {
+
+    let answer = [];
+    let none = 0;
+
+    arr.forEach(elem => {
+        if (elem % divisor === 0) {
+            answer.push(elem);
+        } else {
+            none += 1;
+        }
+    });
+
+    if (none !== 0 && answer.length === 0) {
+        answer.push(-1);
+    }
+
+    answer.sort((a, b) => a - b);
+    
+    return answer;
+}
+
+// 개선된 함수
+function improvedSolution(arr, divisor) {
+
+    let answer = arr.filter(v => v % divisor == 0);
+
+    return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
+}
