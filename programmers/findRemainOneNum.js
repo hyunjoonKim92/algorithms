86a7a10b8b91274a9bcbc5f54d130ed2103c6a0c
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-30 15:03:25 +0900

    -- 220830 커밋 --- 나머지가 1이 되는 수 찾기

---
+++ programmers/findRemainOneNum.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// 나머지가 1이 되는 수 찾기 (프로그래머스)
+function solution(n) {
+    let divisorArr = [];
+
+    for (let i = 1, len = n; i < len; i++) {
+        if (n % i === 1) {
+            divisorArr.push(i);
+        }
+    }
+
+    return Math.min(...divisorArr);
+}
+
+// 개선된 함수
+function improvedSolution(n, x = 1) {
+    while (x++) {
+        if (n % x === 1) {
+            return x;
+        }
+    }
+}
