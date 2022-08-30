4a0d03e59708445a12984f3c19ecb20e45cde101
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-30 14:28:11 +0900

    -- 220830 커밋 --- 부족한 금액 계산하기

---
+++ programmers/calInsufficientAmount.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// 부족한 금액 계산하기 (프로그래머스)
+function solution(price, money, count) {
+    let usePrice = 0;
+
+    for (let i = 1, len = count; i <= len; i++) {
+        usePrice += price * i;
+    }
+
+    return money - usePrice < 0 ? usePrice - money : 0;
+}
+
+// 개선된 함수
+// 가우스 공식
+function improvedSolution(price, money, count) {
+    const calPrice = price * count * (count + 1) / 2 - money;
+
+    return calPrice > 0 ? calPrice : 0;
+}
