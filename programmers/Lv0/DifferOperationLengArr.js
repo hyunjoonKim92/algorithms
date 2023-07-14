c2de209fe0b8c0dd5b99d8663f5beb6f939c9889
hyunjoonKim92 <hyunjoon1992@naver.com>
2023-07-14 16:39:51 +0900

    배열 길이에 따라 다른 연산하기 문제풀이

---
+++ programmers/Lv0/DifferOperationLengArr.js
@@ -0,0 +1,30 @@
+"use strict"
+
+// 배열의 길이에 따라 다른 연산하기 (프로그래머스)
+function solution(arr, n) {
+    let oddIdxArr = [];
+    let evenIdxArr = [];
+
+    arr.forEach((item, index) => {
+        if (index % 2 === 0) {
+            item = item + n;
+        }
+
+        oddIdxArr.push(item);
+    });
+
+    arr.forEach((item, index) => {
+        if (index % 2 !== 0) {
+            item = item + n;
+        }
+
+        evenIdxArr.push(item);
+    });
+
+    return arr.length % 2 === 0 ? evenIdxArr : oddIdxArr;
+}
+
+// improve fun
+const solution = (arr, n) => arr.map((num, idx) => (
+    arr.length % 2 !== idx % 2 ? num + n : num
+));
