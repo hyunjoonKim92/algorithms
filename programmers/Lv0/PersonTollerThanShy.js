32b3a43b7332fd0e318d8fa9dee37b59eff439b3
hyunjoonKim92 <hyunjoon1992@naver.com>
2023-07-16 16:42:51 +0900

    문제풀이 - 머쓱이보다 키 큰 사람

---
+++ programmers/Lv0/PersonTollerThanShy.js
@@ -0,0 +1,21 @@
+"use strict"
+
+// 머쓱이보다 키 큰 사람 (프로그래머스)
+function solution(array, height) {
+    let answer = 0;
+
+    array.forEach(item => {
+        if (item > height) {
+            answer++;
+        }
+    });
+
+    return answer;
+}
+
+// improved fun
+function improvedSolution(array, height) {
+    let answer = array.filter(item => item > height);
+
+    return answer.length;
+}
