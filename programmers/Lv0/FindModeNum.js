41e22e4a171287986df6fc0f0fde5781c12eb6e2
hyunjoonKim92 <hyunjoon1992@naver.com>
2023-08-14 16:27:49 +0900

    문제풀이 - 최빈값 구하기

---
+++ programmers/Lv0/FindModeNum.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// 최빈값 구하기 (프로그래머스)
+function solution(array) {
+    let map = new Map();
+
+    for (let i of array) map.set(i, (map.get(i) || 0) + 1);
+    map = [...map].sort((a, b) => b[1] - a[1]);
+
+    return map.length === 1 || map[0][1] > map[1][1] ? map[0][0] : -1;
+}
