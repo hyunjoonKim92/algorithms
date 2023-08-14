563811882cca4853fd106f46868e9531bd9d2b5a
hyunjoonKim92 <hyunjoon1992@naver.com>
2023-08-14 16:46:23 +0900

    문제풀이 - 짝수는 싫어요

---
+++ programmers/Lv0/HateEvenNum.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// 짝수는 싫어요 (프로그래머스)
+function solution(n) {
+    let result = [];
+
+    for (let i = 1, len = n; i <= len; i++) {
+        if (i % 2 !== 0) {
+            result.push(i);
+        }
+    }
+
+    return result;
+}
+
+// improved fun
+function improvedSoluction(n) {
+    return Array(n).fill(1).map((v, i) => v + i).filter(v => v % 2 === 1);
+}
