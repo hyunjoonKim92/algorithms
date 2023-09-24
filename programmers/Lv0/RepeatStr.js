5d6bfdfe9f6bbbbe20454486719230f0555dfe35
hyunjoonKim92 <hyunjoon1992@naver.com>
2023-09-24 16:22:33 +0900

    문제풀이 - 문자 반복 출력하기

---
+++ programmers/Lv0/RepeatStr.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// 문자 반복 출력하기 - 프로그래머스
+function solution(my_string, n) {
+    const strArr = my_string.split("");
+
+    for (let i = 0; i < strArr.length; i++) {
+        strArr[i] = (strArr[i].repeat(n));
+    }
+
+    return strArr.join("");
+}
+
+// improved fun
+function improvedSolution(my_string, n) {
+    return [...my_string].map(val => val.repeat(n)).join("");
+}
